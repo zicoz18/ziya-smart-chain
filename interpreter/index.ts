aed4043110a65a9fead6c6c2c1d69b6bf14bb800
zicoz18 <zicoz18@ku.edu.tr>
2022-06-30 22:34:12 +0300

    Add LT, GT, EQ, AND, OR operators

--- interpreter/index.ts
+++ interpreter/index.ts
@@ -4,6 +4,11 @@ const SUB = "SUB";
 const MUL = "MUL";
 const DIV = "DIV";
 const PUSH = "PUSH";
+const LT = "LT";
+const GT = "GT";
+const EQ = "EQ";
+const AND = "AND";
+const OR = "OR";
 
 type Code = string | number;
 
@@ -43,6 +48,11 @@ class Interpreter {
 					case SUB:
 					case MUL:
 					case DIV:
+					case LT:
+					case GT:
+					case EQ:
+					case AND:
+					case OR:
 						const a = this.state.stack.pop() as number; // Wrong, fix this. Should check if stack is empty
 						const b = this.state.stack.pop() as number;
 
@@ -52,6 +62,11 @@ class Interpreter {
 						if (opCode === SUB) result = a - b;
 						if (opCode === MUL) result = a * b;
 						if (opCode === DIV) result = a / b;
+						if (opCode === LT) result = a < b ? 1 : 0;
+						if (opCode === GT) result = a > b ? 1 : 0;
+						if (opCode === EQ) result = a === b ? 1 : 0;
+						if (opCode === AND) result = a && b;
+						if (opCode === OR) result = a || b;
 
 						this.state.stack.push(result as number);
 						break;
@@ -85,3 +100,23 @@ console.log("Result of 3 MUL 2 => ", result);
 code = [PUSH, 2, PUSH, 3, DIV, STOP];
 result = new Interpreter().runCode(code);
 console.log("Result of 3 DIV 2 => ", result);
+
+code = [PUSH, 2, PUSH, 3, LT, STOP];
+result = new Interpreter().runCode(code);
+console.log("Result of 3 LT 2 => ", result);
+
+code = [PUSH, 2, PUSH, 3, GT, STOP];
+result = new Interpreter().runCode(code);
+console.log("Result of 3 GT 2 => ", result);
+
+code = [PUSH, 2, PUSH, 2, EQ, STOP];
+result = new Interpreter().runCode(code);
+console.log("Result of 2 EQ 2 => ", result);
+
+code = [PUSH, 1, PUSH, 0, AND, STOP];
+result = new Interpreter().runCode(code);
+console.log("Result of 0 AND 1 => ", result);
+
+code = [PUSH, 1, PUSH, 0, OR, STOP];
+result = new Interpreter().runCode(code);
+console.log("Result of 0 OR 1 => ", result);
